d9b7b4169bba00b6b95d7b915092c70e4ce56ae5
Stacy Vana <98608384+ssvana83@users.noreply.github.com>
2023-06-15 11:59:27 -0400

    HikeForm working

--- src/components/HikeForm.js
+++ src/components/HikeForm.js
@@ -2,28 +2,41 @@ import { useState } from 'react'
 import { useHistory } from 'react'
 
 const HikeForm = () => {
-  const [name, setName] = useState("")
-  const [length, setLength] = useState("")
-  const [difficultyLevel, setDifficultyLevel] = useState("")
-  const [estimatedTime, setEstimatedTime] = useState("")
+  const [hike, setHike] = useState({
+    name: "",
+    length: "",
+    difficultyLevel: "",
+    estimatedTime: ""
+  })
+  // const [length, setLength] = useState("")
+  // const [difficultyLevel, setDifficultyLevel] = useState("")
+  // const [estimatedTime, setEstimatedTime] = useState("")
   
+  const handleChange = (e) => {
+    setHike({
+        ...hike,
+        [e.target.name]: e.target.value
+    })
+  }
+
   const handleSubmit = e => {
     e.preventDefault()
     debugger
   }
 
+
   return (
     <>
-    <div>Create New Hike</div>
+    <h3>Create New Hike</h3>
     <form onSubmit={handleSubmit}>
       <label htmlFor="name">Name</label>
-      <input onChange={} type="text" name="name" value={name} required />
+      <input onChange={handleChange} type="text" name="name" value={hike.name} required /><br />
       <label htmlFor="length">Length (miles)</label>
-      <input onChange={} type="text" name="length" value={length} required />
+      <input onChange={handleChange} type="number" name="length" value={hike.length} required /><br />
       <label htmlFor="difficultyLevel">Difficulty Level</label>
-      <input onChange={} type="text" name="difficultyLevel" value={difficultyLevel} required />
+      <input onChange={handleChange} type="number" name="difficultyLevel" value={hike.difficultyLevel} required /><br />
       <label htmlFor="estimatedTime">Estimated Time (minutes)</label>
-      <input onChange={} type="text" name="estimatedTime" value={estimatedTime} required />
+      <input onChange={handleChange} type="number" name="estimatedTime" value={hike.estimatedTime} required /><br />
       <input type="submit" value="Create Hike" />
 
     </form>
