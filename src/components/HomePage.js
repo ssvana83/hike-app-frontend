e38ed89d0bd2aa433f79dcca80b067e0a6aa8b87
Stacy Vana <98608384+ssvana83@users.noreply.github.com>
2023-06-20 16:26:06 -0400

    homepage styling

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -2,9 +2,9 @@ const HomePage = () => {
   return (
     <div>
       <div>
-        <img src={window.location.origin + "/logo512.png"} alt="default background" />
+        <img src={"https://media.istockphoto.com/id/1189969126/photo/group-of-hikers-walks-in-mountains-at-sunset.jpg?s=612x612&w=0&k=20&c=YlJOZLmELRF0HaEr1Xv4-Uae_VN4vnB8XnE99hGD3gQ="} alt="default background" />
         <div>
-          <p>Our mission is to ...</p>
+          <p>We hope you find our website helpful when searching for your next great hike!</p>
         </div>
       </div>
     </div>
@@ -12,3 +12,4 @@ const HomePage = () => {
 }
 
 export default HomePage
+
