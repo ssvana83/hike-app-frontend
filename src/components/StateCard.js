f5fa8f1c62b2a1ef87850d98145631dff48d1b89
Stacy Vana <98608384+ssvana83@users.noreply.github.com>
2023-06-30 14:11:04 -0400

    state delete button created

--- src/components/StateCard.js
+++ src/components/StateCard.js
@@ -1,7 +1,7 @@
 import {useState, useEffect} from "react"
 import {Link, useParams} from "react-router-dom"
 
-const StateCard = ({ state, hikes }) => {
+const StateCard = ({ state, deleteState, hikes }) => {
   const [stateObject, setStateObject] = useState({});
   const finalState = state ? state : stateObject
     
@@ -10,8 +10,8 @@ const StateCard = ({ state, hikes }) => {
     <div>
       {/* <h3>State Name: <Link to={`/states/${finalState.id}`}>{finalState.statename}</Link></h3>
        */}
-       <h3>State Name: <Link to={`/states/${finalState.id}/hikes`}>{finalState.statename}</Link></h3>
-
+       {/* <h3>State Name: <Link to={`/states/${finalState.id}/hikes`}>{finalState.statename}</Link></h3> */}
+       <Link to={`/states/${state.id}`}>{ state.statename }</Link> - <button onClick={ () => deleteState(state.id) }>Delete</button>
       {/* <h2>Hike Name: {finalState.hikes.name} </h2> */}
       {/* <h4>Town Name: {finalState.townname} </h4> */}
     </div>
